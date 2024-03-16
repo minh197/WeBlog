916c148243346dd2d3d9a242e40801c7c6fb1ee8
Minh Nguyen <139167959+mnguyenKU@users.noreply.github.com>
2024-03-16 10:34:26 -0500

    Fix minor typo

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,7 +3,7 @@ import './Card.css'
 const Card = () => {
   return (
       <div className='card'>
-        <h2 className='m-0 p-0'>Sameple Card Name</h2>
+        <h2 className='m-0 p-0'>Sample Card Name</h2>
         <p className="date">March 16, 2024</p>
         <p>This is card description</p>
         <a className="read-more">Read more</a>
