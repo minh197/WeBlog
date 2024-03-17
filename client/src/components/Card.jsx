ac7ebeadfe09a952b36240c313b63ce1376ffeb6
Minh Nguyen <139167959+mnguyenKU@users.noreply.github.com>
2024-03-17 13:08:47 -0500

    Final touch for card component

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,13 +3,17 @@ import './Card.css'
 import SampleImage from './images/img-1.avif'
 const Card = () => {
   return (
-    <div className='card-list'>
+    <div className="card-list">
       <div className="card">
-        <h2 className="m-0 p-0">Sample Card Name</h2>
-        <p className="date">March 16, 2024</p>
-        <img className='card-img' src={SampleImage} alt="CardImage"/>
-        <p>This is card description</p>
-        <a className="read-more">Read more</a>
+        <div className="card-img">
+          <img src={SampleImage} alt="CardImage" />
+        </div>
+        <div className='card-content'>
+          <h2 className="m-0 p-0">Sample Card Name</h2>
+          <p className="date">March 16, 2024</p>
+          <p>This is card description</p>
+          <a className="read-more">Read more</a>
+        </div>
       </div>
     </div>
   );
